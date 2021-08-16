809b40d5f010696b8a801bc92af248e85dc54a7f
riderx <martindonadieu@gmail.com>
2021-08-16 20:15:28 +0200

    fix: base url in vite config

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -6,6 +6,7 @@ import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: resolve(process.cwd(), 'demo'),
+  base: '/vue-timer-hook/',
   build: {
     outDir: '../demo_dist',
     rollupOptions: {
